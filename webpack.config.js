22135d28a425d19db87426fbdb27234b1027065f
132yse <1533540012@qq.com>
2018-08-26 22:06:43 +0800

    日常提交

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'fre.js',
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
